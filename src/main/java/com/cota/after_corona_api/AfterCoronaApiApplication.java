5fc0e4475966a88d7d3b0bc8c49a8332b984492d
지인호 <77221262+key-del-jeeinho@users.noreply.github.com>
2021-12-18 13:17:26 +0900

    Initial commit with spring boot and gradle

---
+++ src/main/java/com/cota/after_corona_api/AfterCoronaApiApplication.java
@@ -0,0 +1,13 @@
+package com.cota.after_corona_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AfterCoronaApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AfterCoronaApiApplication.class, args);
+    }
+
+}
